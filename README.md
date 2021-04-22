3c0831807b7078b22ffc073c2eed69a0bd67f207
sidecus <4399408+sidecus@users.noreply.github.com>
2021-04-22 15:41:02 +0800

    add parameter to control http mode or grpc mode

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 
 # Start producer:
-dapr run -a producer -d ./components go run producer/main.go
+```dapr run -a producer go run producer/main.go```
 
-# Start Consumer:
-dapr run -a consumer -P http -p 27015 -d ./components go run consumer/main.go
+# Start consumer:
+## HTTP:
+```dapr run -a consumer -P http -p 27015 go run consumer/main.go```
+## or gRPC:
+```dapr run -a consumer -P gRPC -p 27015 go run consumer/main.go gRPC```
--- consumer/main.go
+++ consumer/main.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"encoding/binary"
 	"log"
+	"os"
 
 	"github.com/dapr/go-sdk/service/common"
 	"github.com/pkg/errors"
 
-	//daprd "github.com/dapr/go-sdk/service/grpc"
-	// Comment out below line and uncomment above line to use grpc
-	daprd "github.com/dapr/go-sdk/service/http"
+	daprd "github.com/dapr/go-sdk/service/grpc"
+	daprdhttp "github.com/dapr/go-sdk/service/http"
 )
 
 var (
@@ -21,16 +21,21 @@ var (
 )
 
 func main() {
+	mode := parseArgs()
+
+	// Event handler
 	eventHandler := func(ctx context.Context, in *common.TopicEvent) (retry bool, err error) {
 		log.Printf("Received event %s from source %s. DateType: %s", in.ID, in.Source, in.DataContentType)
 
 		if in.DataContentType != "text/plain" {
+			log.Print("Invalid content type received")
 			return false, errors.Errorf("Invalid content type %s (eventID: %s, source: %s)", in.DataContentType, in.ID, in.Source)
 		}
 
 		topic := in.Topic
 		data := []byte(in.Data.(string))
 		if len(data) != 8 {
+			log.Printf("Invalid data length, %v", data)
 			return false, errors.Errorf("Invalid payload data (eventID: %s, source: %s)", in.ID, in.Source)
 		}
 
@@ -41,14 +46,20 @@ func main() {
 		return
 	}
 
+	// Create service
+	var s common.Service
 	var err error = nil
-	// s, err := daprd.NewService(serviceAddress)
-	// Comment out below line and uncomment above line to use grpc
-	s := daprd.NewService(serviceAddress)
+	if mode == "http" {
+		s = daprdhttp.NewService(serviceAddress)
+	} else {
+		s, err = daprd.NewService(serviceAddress)
+	}
+
 	if err != nil {
 		log.Fatal("Service creation failed")
 	}
 
+	// Subscribe to counter topic
 	log.Print("Starting service")
 	sub := &common.Subscription{
 		PubsubName: pubsubName,
@@ -58,7 +69,22 @@ func main() {
 	if err := s.AddTopicEventHandler(sub, eventHandler); err != nil {
 		log.Fatalf("error adding topic subscription: %v", err)
 	}
+
+	// Start service
 	if err := s.Start(); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
+}
+
+func parseArgs() string {
+	if len(os.Args) < 2 {
+		return "http"
+	}
+
+	mode := os.Args[1]
+	if mode != "http" && mode != "gRPC" {
+		log.Fatalf("Invalid mode: %s", mode)
+	}
+
+	return mode
 }
